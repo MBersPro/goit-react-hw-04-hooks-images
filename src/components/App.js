be08db4376c6f86a221882ee0a17657fabb05ccb
MBers <mbers.pro@gmail.com>
2021-11-12 11:07:18 +0300

    Redone

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,6 @@ import Modal from "./modal/Modal";
 import Loader from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
-
 const initialState = {
   name: "",
   imgList: [],
@@ -15,20 +14,20 @@ const initialState = {
   loader: false,
   isModalOpen: false,
   modalImg: "",
-}
+};
 
 const App = () => {
   const [newState, setState] = useState(initialState);
 
   const onModalOpen = (modalImg) => {
-    setState((prev) => ({...prev, isModalOpen: true, modalImg: modalImg }));
+    setState((prev) => ({ ...prev, isModalOpen: true, modalImg: modalImg }));
   };
 
   const onModalClose = (e) => {
     if (e.code !== "Escape") {
       return;
     }
-    setState((prev) => ({...prev, isModalOpen: false, modalImg: "" }));
+    setState((prev) => ({ ...prev, isModalOpen: false, modalImg: "" }));
   };
 
   const onSubmit = (q) => {
@@ -49,41 +48,41 @@ const App = () => {
           }))
         )
         .finally(() => {
-          window.scrollTo({
-            top: document.documentElement.scrollHeight,
-            behavior: "smooth",
-          });
+          if (newState.page !== 1) {
+            window.scrollTo({
+              top: document.documentElement.scrollHeight,
+              behavior: "smooth",
+            });
+          }
         });
     }
-  }, [newState.name, newState.page])
+  }, [newState.name, newState.page]);
 
   const onLoadMore = () => {
-    setState((prev) => ({...prev,
-      page: prev.page + 1,
-    }));
+    setState((prev) => ({ ...prev, page: prev.page + 1 }));
   };
 
-    return (
-      <>
-        <Searchbar onSubmit={onSubmit} />
-        <ImageGallery imgList={newState.imgList} onModalOpen={onModalOpen} />
-        {newState.imgList.length > 0 && <Button onLoadMore={onLoadMore} />}
-        {newState.isModalOpen && (
-          <Modal modalImg={newState.modalImg} onModalClose={onModalClose} />
-        )}
+  return (
+    <>
+      <Searchbar onSubmit={onSubmit} />
+      <ImageGallery imgList={newState.imgList} onModalOpen={onModalOpen} />
+      {newState.imgList.length > 0 && <Button onLoadMore={onLoadMore} />}
+      {newState.isModalOpen && (
+        <Modal modalImg={newState.modalImg} onModalClose={onModalClose} />
+      )}
 
-        {newState.loader && (
-          <Loader
-            type="Puff"
-            color="#001aff"
-            height={200}
-            width={200}
-            timeout={3000}
-            position="top-center"
-          />
-        )}
-      </>
-    );
-}
+      {newState.loader && (
+        <Loader
+          type="Puff"
+          color="#001aff"
+          height={200}
+          width={200}
+          timeout={3000}
+          position="top-center"
+        />
+      )}
+    </>
+  );
+};
 
 export default App;
